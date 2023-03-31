21d98ac2c5519ff7d70c109117407f2cf201b4db
Anishkumar27 <53423892+Anishkumar27@users.noreply.github.com>
2023-04-01 00:41:33 +0530

    Update middlewares.go

--- middlewares/middlewares.go
+++ middlewares/middlewares.go
@@ -4,8 +4,8 @@ import (
 	"errors"
 	"net/http"
 
-	"github.com/victorsteven/fullstack/api/auth"
-	"github.com/victorsteven/fullstack/api/responses"
+	"github.com/victorsteven/fullstack/auth"
+	"github.com/victorsteven/fullstack/responses"
 )
 
 func SetMiddlewareJSON(next http.HandlerFunc) http.HandlerFunc {
