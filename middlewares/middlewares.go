a0f543c64fd1b249bd450461f0560c336f3e1632
Anishkumar27 <53423892+Anishkumar27@users.noreply.github.com>
2023-04-01 12:39:53 +0530

    Update middlewares.go

--- middlewares/middlewares.go
+++ middlewares/middlewares.go
@@ -25,3 +25,19 @@ func SetMiddlewareAuthentication(next http.HandlerFunc) http.HandlerFunc {
 		next(w, r)
 	}
 }
+
+func CORS(next http.HandlerFunc) http.HandlerFunc {
+  return func(w http.ResponseWriter, r *http.Request) {
+    w.Header().Add("Access-Control-Allow-Origin", "*")
+    w.Header().Add("Access-Control-Allow-Credentials", "true")
+    w.Header().Add("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+    w.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+
+    if r.Method == "OPTIONS" {
+        http.Error(w, "No Content", http.StatusNoContent)
+        return
+    }
+
+    next(w, r)
+  }
+}
